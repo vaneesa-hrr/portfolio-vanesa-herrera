079939967b1e87ee10a864d9128506948c91d564
vaneesa.hrr <alumno.415193@iesazarquiel.es>
2025-01-20 13:01:33 +0100

    intento de hcaer un formulario de google sheets (fallido)

--- links.js
+++ links.js
@@ -23,5 +23,19 @@ function closeMenu() {
   sideMenu.style.right = "-200px";
 }
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxlky3mC3O6djoZfy2xA1x7Umi0oPJdg_vjtps7nkTEqUkGBfvVh8jtLDRvvfWzOL3F/exec'
+const form = document.forms['submit-to-google-sheet'];
+
+if (form) {
+  form.addEventListener('submit', e => {
+    e.preventDefault();
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message));
+  });
+} else {
+  console.error("Form not found!");
+}
+
 
 
