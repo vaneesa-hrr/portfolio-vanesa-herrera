b33e1334270ba95d03e0392878d6fa05b1ccaea6
vaneesa.hrr <alumno.415193@iesazarquiel.es>
2025-01-29 10:18:20 +0100

    mostrar que el mensaje se ha enviado correctamente del formulario

--- links.js
+++ links.js
@@ -26,13 +26,18 @@ function closeMenu() {
 document.addEventListener('DOMContentLoaded', () => {
   const scriptURL = 'https://script.google.com/macros/s/AKfycbyYbNpBcBqBEUWhlDeuqgaFsflhtqBY3gAfhmRIrnGJruwGIgKkohA3ysnf7PAgcMEQ/exec';
   const form = document.forms['submit-to-google-sheet'];
+  const msg = document.getElementById("msg");
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Success:', data);
+      .then((response) => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout
+          (function () {
+            msg.innerHTML = ""
+          }, 5000)
+        form.reset()
       })
       .catch((error) => {
         console.error('Error:', error);
